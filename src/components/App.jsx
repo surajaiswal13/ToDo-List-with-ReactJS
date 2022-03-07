6570a5c3a75448fc1b8be6f7c68853fb8a699c17
Suraj Jaiswal <surajaiswal13@gmail.com>
2022-03-07 23:00:05 +0530

    [Update] Realtime State Change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [item, setItem] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value
+    setItem(newValue);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input onChange={handleChange} type="text" />
         <button>
           <span>Add</span>
         </button>
